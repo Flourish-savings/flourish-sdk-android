123770e83461cefb56aa26ead8d33ecbc196a308
Rafael Kerr <rafael.kerr@gmail.com>
2024-03-01 19:23:54 -0300

    Adding initial implementation of library

---
+++ app/flourish_sdk/src/main/java/com/flourish/sdk/android/RewardsActivity.kt
@@ -0,0 +1,23 @@
+package com.flourish.sdk.android
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.flourish.sdk.android.databinding.RewardsMainBinding
+
+class RewardsActivity : AppCompatActivity() {
+
+    private lateinit var binding: RewardsMainBinding
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = RewardsMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.flourishApp.loadUrl("https://platform-stg.flourishfi.com/?lang=es&token=eyJhbGciOiJIUzI1NiJ9.eyJjbGllbnRfaWQiOjIsInVzZXJfaWQiOjM3LCJleHAiOjE3MDk0OTM2OTJ9.6Ln_AI65XR6xPgbNH_K2DB3I5zZx7TyARvUT37nWCUw")
+
+        binding.flourishApp.settings.javaScriptEnabled = true
+    }
+}
